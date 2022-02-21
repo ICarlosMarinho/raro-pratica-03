008f97543915799cf27692a03a266c6f675103cf
Carlos Marinho <carlos.marinho20@outlook.com>
2022-02-21 15:26:27 -0300

    Add fourth, fifth and sixth questions answer functions.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { getImaginaryPart, getRealPart, hasInvalidLenght } from "./utils";
+import { getImaginaryPart, getRealPart, hasInvalidLength, bubbleSort, removeRecurrences, has } from "./utils";
 
 /*
  * Faça um algoritmo que calcule a fórmula de equação quadrática ("fórmula de bháskara").
@@ -29,7 +29,7 @@ const calcSolution = (a: number = 1, b: number = 0, c: number = 0): Solution => 
  * forma um `triângulo equilátero`, um `triângulo isósceles` ou um triângulo escaleno.
  */
 const getTriangleType = (a: number, b: number, c: number): TriangleType => {
-  if (hasInvalidLenght(a, b, c)) {
+  if (hasInvalidLength(a, b, c)) {
     throw new Error("Os lados devem ser maiores que zero.");
   }
 
@@ -42,4 +42,56 @@ const getTriangleType = (a: number, b: number, c: number): TriangleType => {
   return "Escaleno";
 };
 
-debugger;
+/*
+ * Faça um algoritmo que recebe um array de numeros, e retorne um novo array, com os objetos ordenados.
+ */
+const getSortedArray = (numbers: number[]): number[] => {
+  const copy = [...numbers];
+
+  bubbleSort(copy, copy.length);
+
+  return copy;
+};
+
+/*
+ * Implementar a união dos grupos a e b.
+ */
+const getUnion = (firstArray: number[], secondArray: number[]): number[] => {
+  const result = [...firstArray, ...secondArray];
+
+  return removeRecurrences(result);
+};
+
+/*
+ * Implementar a interseção dos gupos a e b.
+ */
+const getIntersection = (firstArray: number[], secondArray: number[]): number[] => {
+  const result: number[] = [];
+
+  for (let element of firstArray) {
+    if (has(secondArray, element)) {
+      result.push(element);
+    }
+  }
+
+  return removeRecurrences(result);
+};
+
+/*
+ * Implementar a diferença dos gupos a e b.
+ * OBS.: Não ficou claro para mim se a questão pede "a - b" ou "b - a",
+ * então, eu implementei a diferença simétrica entre os conjuntos.
+ */
+const getDifference = (firstArray: number[], secondArray: number[]): number[] => {
+  const union = getUnion(firstArray, secondArray);
+  const intersection = getIntersection(firstArray, secondArray);
+  const result: number[] = [];
+
+  for (let element of union) {
+    if (!has(intersection, element)) {
+      result.push(element);
+    }
+  }
+
+  return result;
+};
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,7 +2,7 @@ export const getRealPart = (a: number, b: number) => (-1 * b) / (2 * a);
 
 export const getImaginaryPart = (a: number, delta: number) => Math.sqrt(-1 * delta) / (2 * a);
 
-export const hasInvalidLenght = (...lenghts: number[]): boolean => {
+export const hasInvalidLength = (...lenghts: number[]): boolean => {
   for (let length of [...lenghts]) {
     if (length <= 0) {
       return true;
@@ -10,4 +10,40 @@ export const hasInvalidLenght = (...lenghts: number[]): boolean => {
   }
 
   return false;
+};
+
+export const bubbleSort = (numbers: number[], length: number): void => {
+  if (length < 2) return;
+
+  const lastIndex = length - 1;
+
+  for (let i = 0; i < lastIndex; i++) {
+    if (numbers[i] > numbers[i + 1]) {
+      [numbers[i], numbers[i + 1]] = [numbers[i + 1], numbers[i]];
+    }
+  }
+
+  bubbleSort(numbers, lastIndex);
+};
+
+export const has = (array: number[], value: number): boolean => {
+  for (let element of array) {
+    if (element === value) {
+      return true;
+    }
+  }
+
+  return false;
+};
+
+export const removeRecurrences = (numbers: number[]): number[] => {
+  const result: number[] = [];
+
+  for (let number of numbers) {
+    if (!has(result, number)) {
+      result.push(number);
+    }
+  }
+
+  return result;
 };
